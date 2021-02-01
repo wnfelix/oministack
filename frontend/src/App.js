4498f2e98a5a4c8e712c390756e73dc79ca9173c
William N Felix <william.nfelix@gmail.com>
2021-02-01 20:17:04 -0300

    inclusão do frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './global.css';
+import Routes from './routes';
+
+/**
+ * jsx (javascript xml)
+ */
+
+function App() {
+  
+
+  return (
+    <Routes />
+  );
+}
+
+export default App;
